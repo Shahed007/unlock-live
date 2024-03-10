4e7155f77a9a7b90c7bdf13e8c419a43b27e0dff
Shahed <sk0178397@gmial.com>
2024-03-10 17:56:27 +0600

    Added slider functionality on new recipes section.

--- src/pages/home/NewRecipes.tsx
+++ src/pages/home/NewRecipes.tsx
@@ -1,12 +1,40 @@
+"use client";
 import PrimaryTitle from "@/components/title/PrimaryTitle";
 import sliderImage1 from "@/assets/images/slider-image-1.png";
 import Image from "next/image";
+import { Navigation, Pagination } from "swiper/modules";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import { MutableRefObject, useRef, useState } from "react";
 
 const NewRecipes = () => {
+  const swiperRef: MutableRefObject<any> = useRef(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePrev = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex((prev) => prev - 1);
+      if (swiperRef.current) {
+        swiperRef.current.swiper.slidePrev();
+      }
+    }
+  };
+
+  const handleNext = () => {
+    if (currentIndex < swiperRef.current.swiper.slides.length - 1) {
+      if (swiperRef.current) {
+        swiperRef.current.swiper.slideNext();
+      }
+    }
+  };
   return (
-    <section className="py-20 bg-[#FFFAF2]">
-      <div className="container grid grid-cols-1 md:grid-cols-2 gap-6">
-        <div className="">
+    <section className="py-20 bg-[#FFFAF2] overflow-hidden">
+      <div className=" grid grid-cols-3 gap-6 pl-16">
+        <div className="col-span-1">
           <h4 className="flex items-center gap-2 before:inline-block before:h-[2px] before:w-[50px] before:bg-gradient-to-l before:from-secondary_color before:to-secondary_color/10 text-base text-secondary_color italic">
             New Recipes
           </h4>
@@ -28,7 +56,16 @@ const NewRecipes = () => {
             </p>
           </div>
           <div className="flex items-center gap-6 mt-10">
-            <button className="hover:bg-primary_color hover:text-white duration-300 active:scale-95 h-10 w-10 border-[1.5px] text-primary_color flex justify-center items-center border-primary_color rounded-full">
+            <button
+              disabled={currentIndex === 0}
+              onClick={handlePrev}
+              id="prev-btn"
+              className={`hover:bg-primary_color cursor-pointer hover:text-white duration-300 active:scale-95 h-10 w-10 border-[1.5px] text-primary_color flex justify-center items-center border-primary_color rounded-full ${
+                currentIndex === 0
+                  ? "disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:text-primary_color disabled:scale-100"
+                  : " "
+              }`}
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -44,7 +81,21 @@ const NewRecipes = () => {
                 />
               </svg>
             </button>
-            <button className="hover:bg-primary_color hover:text-white duration-300 active:scale-95 h-10 w-10 border-[1.5px] text-primary_color flex justify-center items-center border-primary_color rounded-full">
+            <button
+              disabled={
+                currentIndex === swiperRef.current?.swiper.slides.length - 1
+              }
+              onClick={handleNext}
+              id="next-btn"
+              className={`
+              hover:bg-primary_color hover:text-white duration-300 active:scale-95 h-10 w-10 border-[1.5px] text-primary_color flex justify-center items-center border-primary_color rounded-full
+              ${
+                currentIndex === swiperRef.current?.swiper.slides.length - 1
+                  ? "disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:text-primary_color disabled:scale-100"
+                  : ""
+              }
+              `}
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -62,18 +113,120 @@ const NewRecipes = () => {
             </button>
           </div>
         </div>
-        <div >
-          <div className="relative rounded-[28px] overflow-hidden group">
-            <Image className="h-full w-full object-cover" src={sliderImage1} alt="slider image 1"/>
-            <div className="absolute group-hover:p-3 group-hover:h-full  top-0 left-0 w-full duration-300 p-0  h-0  bg-gradient-to-l from-[#FD990000]/10 to-[#FFFFFF]">
-              <div className="bg-gradient-to-b flex flex-col justify-end rounded-[28px] from-[#101113]/10 to-[#101113] w-full h-full">
-                <ul className="text-white list-disc font-openSans text-base space-y-3 px-10 pb-10">
-                  <li>Pita bread or wraps</li>
-                  <li>Toppings: tomatoes, tahini sauce, etc.</li>
-                </ul>
+        <div className="col-span-2  h-full w-full flex items-center">
+          <Swiper
+            ref={swiperRef}
+            modules={[Navigation]}
+            spaceBetween={30}
+            slidesPerView={3}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log("slide change")}
+            navigation={{ prevEl: "#prev-btn", nextEl: "#next-btn" }}
+            className=" "
+          >
+            <SwiperSlide className="h-full w-full ml-20 ">
+              <div className="relative rounded-[28px] h-full overflow-hidden group">
+                <Image
+                  className="h-full w-full object-cover"
+                  src={sliderImage1}
+                  alt="slider image 1"
+                />
+                <div className="absolute p-3 h-full opacity-0 group-hover:opacity-100  top-0 left-0 w-full duration-500   bg-gradient-to-l from-[#FD990000]/10 to-[#FFFFFF]">
+                  <div className="bg-gradient-to-b flex flex-col justify-end rounded-[28px] from-[#101113]/10 to-[#101113] w-full h-full">
+                    <ul className="text-white list-disc font-openSans text-sm space-y-3 px-5 pb-10">
+                      <li>Pita bread or wraps</li>
+                      <li>Toppings: tomatoes, tahini sauce, etc.</li>
+                    </ul>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="relative rounded-[28px] overflow-hidden group">
+                <Image
+                  className="h-full w-full object-cover"
+                  src={sliderImage1}
+                  alt="slider image 1"
+                />
+                <div className="absolute p-3 h-full opacity-0 group-hover:opacity-100  top-0 left-0 w-full duration-500   bg-gradient-to-l from-[#FD990000]/10 to-[#FFFFFF]">
+                  <div className="bg-gradient-to-b flex flex-col justify-end rounded-[28px] from-[#101113]/10 to-[#101113] w-full h-full">
+                    <ul className="text-white list-disc font-openSans text-sm space-y-3 px-5 pb-10">
+                      <li>Pita bread or wraps</li>
+                      <li>Toppings: tomatoes, tahini sauce, etc.</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="relative rounded-[28px] overflow-hidden group">
+                <Image
+                  className="h-full w-full object-cover"
+                  src={sliderImage1}
+                  alt="slider image 1"
+                />
+                <div className="absolute p-3 h-full opacity-0 group-hover:opacity-100  top-0 left-0 w-full duration-500   bg-gradient-to-l from-[#FD990000]/10 to-[#FFFFFF]">
+                  <div className="bg-gradient-to-b flex flex-col justify-end rounded-[28px] from-[#101113]/10 to-[#101113] w-full h-full">
+                    <ul className="text-white list-disc font-openSans text-sm space-y-3 px-5 pb-10">
+                      <li>Pita bread or wraps</li>
+                      <li>Toppings: tomatoes, tahini sauce, etc.</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="relative rounded-[28px] overflow-hidden group">
+                <Image
+                  className="h-full w-full object-cover"
+                  src={sliderImage1}
+                  alt="slider image 1"
+                />
+                <div className="absolute p-3 h-full opacity-0 group-hover:opacity-100  top-0 left-0 w-full duration-500   bg-gradient-to-l from-[#FD990000]/10 to-[#FFFFFF]">
+                  <div className="bg-gradient-to-b flex flex-col justify-end rounded-[28px] from-[#101113]/10 to-[#101113] w-full h-full">
+                    <ul className="text-white list-disc font-openSans text-sm space-y-3 px-5 pb-10">
+                      <li>Pita bread or wraps</li>
+                      <li>Toppings: tomatoes, tahini sauce, etc.</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="relative rounded-[28px] overflow-hidden group">
+                <Image
+                  className="h-full w-full object-cover"
+                  src={sliderImage1}
+                  alt="slider image 1"
+                />
+                <div className="absolute p-3 h-full opacity-0 group-hover:opacity-100  top-0 left-0 w-full duration-500   bg-gradient-to-l from-[#FD990000]/10 to-[#FFFFFF]">
+                  <div className="bg-gradient-to-b flex flex-col justify-end rounded-[28px] from-[#101113]/10 to-[#101113] w-full h-full">
+                    <ul className="text-white list-disc font-openSans text-sm space-y-3 px-5 pb-10">
+                      <li>Pita bread or wraps</li>
+                      <li>Toppings: tomatoes, tahini sauce, etc.</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="relative rounded-[28px] overflow-hidden group">
+                <Image
+                  className="h-full w-full object-cover"
+                  src={sliderImage1}
+                  alt="slider image 1"
+                />
+                <div className="absolute p-3 h-full opacity-0 group-hover:opacity-100  top-0 left-0 w-full duration-500   bg-gradient-to-l from-[#FD990000]/10 to-[#FFFFFF]">
+                  <div className="bg-gradient-to-b flex flex-col justify-end rounded-[28px] from-[#101113]/10 to-[#101113] w-full h-full">
+                    <ul className="text-white list-disc font-openSans text-sm space-y-3 px-5 pb-10">
+                      <li>Pita bread or wraps</li>
+                      <li>Toppings: tomatoes, tahini sauce, etc.</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          </Swiper>
         </div>
       </div>
     </section>
